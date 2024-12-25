fe5514c76d33f16f0ff30b342e7346c3ed8f5536
rahultejus <rahultejus123@gmail.com>
2024-12-26 00:16:24 +0530

    updation in color of h1 and h3

--- 1-Theme_Changer/script.js
+++ 1-Theme_Changer/script.js
@@ -2,13 +2,23 @@
 
 const buttons=document.querySelectorAll('.button')
 const body=document.querySelector('body')
-
+const heading3=document.querySelector('h3')
+const heading1=document.querySelector('h1')
+console.log(heading3)
 buttons.forEach(function(button) {
     console.log(button);
     button.addEventListener('click',function(e){
-    //    console.log(e)
+       console.log(e)
        console.log(e.target);
        console.log(e.target.id);
        body.style.backgroundColor =e.target.id;
+
+       if(e.target.id =='black'){
+        heading3.style.color='white';
+        heading1.style.color='white';
+        
+
+       }
     })
 });
+
